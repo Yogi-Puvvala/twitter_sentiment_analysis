ce0f1bdb641053da3864f7bb288b63af6a28f082
Yogi Puvvala <yogipuvvala333@gmail.com>
2026-03-20 18:06:30 +0530

    Almost done

--- api/app.py
+++ api/app.py
@@ -4,16 +4,13 @@ from src.predict import predict as get_prediction
 
 app = FastAPI(title="Twitter Sentiment Analysis API")
 
-# Request schema
 class TweetInput(BaseModel):
     tweet: str
 
-# Health check 
 @app.get("/")
 def health():
     return {"message": "Welcome to Twitter Sentiment Analysis"}
 
-# Predict endpoint 
 @app.post("/predict/")
 def predict_tweet(data: TweetInput):
     result = get_prediction(data.tweet)
